6ea3318589b8e2e1db7eaed5495172e90f0ef122
Longines <liko@5bsoftware.com>
2022-04-22 13:49:12 +0800

    update sleep time

--- utils/run.js
+++ utils/run.js
@@ -85,7 +85,7 @@ module.exports = async (
 				}
 			);
 		}
-		await sleep(500);
+		await sleep(100);
 		/*await execa(`npm`, [`install`, ...pkgs]);
     await execa(`npm`, [`install`, `prettier`, `-D`]);
     await execa(`npm`, [`dedupe`]);*/
